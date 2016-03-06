813f3fdd9a05c70dacc3da90606f2864b6f27cc9
cloudcray <cloudcray@gmail.com>
2016-03-05 22:05:32 -0500

    Added DB

--- contracts/__init__.py
+++ contracts/__init__.py
@@ -1,14 +1,18 @@
 __author__ = 'Cloud'
 from flask import Flask
 from config import config
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+db = SQLAlchemy()
 
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
+
+    db.init_app(app)
 
     from .base import bp
 
